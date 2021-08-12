3320812343c3434fd01e7e4c0d3b382f219a2ef5
florvhee <florvhee@gmail.com>
2021-08-12 19:02:05 +0200

    debug

--- locasix/models/order.py
+++ locasix/models/order.py
@@ -4,4 +4,5 @@ class Order(models.Model):
     _name = "sale.order"
     _inherit = "sale.order"
 
-    
+    front_page_body = fields.Html(string="Page de garde")
+    added_terms = fields.Html(string="Conditions additionnelles")
