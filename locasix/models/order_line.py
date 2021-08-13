3cfd12596ced1636023b97aad3fcad4654d45907
florvhee <florvhee@gmail.com>
2021-08-13 18:23:05 +0200

    debug

--- locasix/models/order_line.py
+++ locasix/models/order_line.py
@@ -1,7 +1,16 @@
 from odoo import fields, api, models
 
+import logging
+_logger = logging.getLogger(__name__)
+
+
 class OrderLine(models.Model):
     _name = "sale.order.line"
     _inherit = "sale.order.line"
 
     has_ref_to_condi = fields.Boolean(string="cond. add.", default=False)
+
+    def _prepare_add_missing_fields(self, values):
+        _logger.info("YOYO")
+        _logger.info(values)
+        return super(OrderLine, self)._prepare_add_missing_fields(values)
