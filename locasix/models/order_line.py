ef9a9981e78e27d48356dbf413ee3348b364a4b2
florvhee <florvhee@gmail.com>
2021-08-13 15:02:14 +0200

    debug

--- locasix/models/order_line.py
+++ locasix/models/order_line.py
@@ -4,4 +4,4 @@ class OrderLine(models.Model):
     _name = "sale.order.line"
     _inherit = "sale.order.line"
 
-    has_ref_to_condi = fields.Boolean(string="Référence aux conditons additionnelles", default=False)
+    has_ref_to_condi = fields.Boolean(string="cond. add.", default=False)
