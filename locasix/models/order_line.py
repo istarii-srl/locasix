8c1a86ee97568ff56e7b3b692ec3ec5f8f6ef153
florvhee <florvhee@gmail.com>
2021-09-05 20:46:16 +0200

    debug

--- locasix/models/order_line.py
+++ locasix/models/order_line.py
@@ -57,7 +57,7 @@ class OrderLine(models.Model):
         if vals.get('product_id', False) and not vals.get("from_update", False):
             vals.pop("from_update", 1)
             res = super(OrderLine, self).write(vals)
-            #self.update_line_values()
+            self.update_line_values()
             
         else:
             vals.pop("from_update", 1)
@@ -70,7 +70,7 @@ class OrderLine(models.Model):
         _logger.info("in create order lines")
         _logger.info(vals)
         obj = super(OrderLine, self).create(vals)
-        #obj.update_line_values()
+        obj.update_line_values()
         return obj
 
 
