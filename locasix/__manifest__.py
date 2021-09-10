7a4a74b23476dacdb263efedf0b35eb27133ec6e
thherman <hermansthibault@gmail.com>
2021-09-10 16:54:37 +0200

    debug

--- locasix/__manifest__.py
+++ locasix/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "locasix",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Locasix""",
 
     'description': """
         Long description of module's purpose
