6a6068f198ea4f9e11cd96a836152a84bb249705
florvhee <florvhee@gmail.com>
2021-09-17 08:45:41 +0200

    debug

--- locasix/__manifest__.py
+++ locasix/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'ERP',
-    'version': '0.16.19',
+    'version': '0.16.20',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale_management', 'product'],
@@ -35,6 +35,7 @@
         'views/product_template.xml',
         'views/category.xml',
         'views/order.xml',
+        'views/assets.xml',
         'views/client.xml',
         'wizards/export_products.xml',
         'wizards/import_products.xml',
--- locasix/static/src/js/product_tree_buttons.js
+++ locasix/static/src/js/product_tree_buttons.js
@@ -1,3 +1,4 @@
+/**
 odoo.define("locasix.product.buttons", function (require) {
     "use strict";
     var ListController = require("web.ListController");
@@ -54,3 +55,29 @@ odoo.define("locasix.product.buttons", function (require) {
     viewRegistry.add("locasix_product_tree_buttons", ProductTreeButtonsListView);
   });
   
+ */
+  odoo.define('locasix.product_tree_view_button', function (require){
+    "use strict";
+
+    var ajax = require('web.ajax');
+    var ListController = require('web.ListController');
+
+    ListController.include({
+        renderButtons: function($node) {
+            this._super.apply(this, arguments);
+            var self = this;
+            if (this.$buttons) {
+                $(this.$buttons).find('.o_button_export_product').on('click', function() {
+                  self.do_action({
+                    name: "Exporter les produits",
+                    type: "ir.actions.act_window",
+                    res_model: "locasix.product.export",
+                    target: "new",
+                    views: [[false, "form"]],
+                    context: { is_modal: true },
+                  });                 //custom code
+                });
+            }
+        },
+    });
+});
