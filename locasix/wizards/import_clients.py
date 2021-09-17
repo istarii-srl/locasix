a30d6f1d2a89d8c731a163967ce2cd4336b687cd
florvhee <florvhee@gmail.com>
2021-09-17 15:55:05 +0200

    import clients

--- locasix/wizards/import_clients.py
+++ locasix/wizards/import_clients.py
@@ -1,4 +1,12 @@
 from odoo import api, fields, models
+import xlrd
+import tempfile
+import binascii
+from odoo.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
+
 
 
 class ImportClients(models.TransientModel):
@@ -9,5 +17,58 @@ class ImportClients(models.TransientModel):
 
 
     def import_clients(self):
-        for wizard in self:
-            return
+        _logger.info("Import clients")
+        try:
+            for wizard in self:
+                fp = tempfile.NamedTemporaryFile(suffix=".xls")
+                fp.write(binascii.a2b_base64(wizard.file))
+                fp.seek(0)
+                book = xlrd.open_workbook(fp.name)
+                sheet = book.sheet_by_index(0)
+                lines = []
+                for i in range(10, sheet.nrows):
+                    lines.append({
+                        "company_name": sheet.cell_value(i, 1), 
+                        "street": sheet.cell_value(i, 3), 
+                        "country/code": sheet.cell_value(i, 4),
+                        "municipality": sheet.cell_value(i, 5),
+                        "contact_name": sheet.cell_value(i, 6),
+                        "TVA": sheet.cell_value(i, 8),
+                        "company_title": sheet.cell_value(i, 13),
+                        "phone": sheet.cell_value(i, 14),
+                        "mobile": sheet.cell_value(i, 16),
+                        "email": sheet.cell_value(i, 17),
+                        "notes": sheet.cell_value(i, 18),
+                        "email_compta": sheet.cell_value(i, 25),
+                        "condi_payment": sheet.cell_value(i, 56),
+                    })
+                for line in lines:
+                    real_company_name = line["company_name"] + line["company_title"] if line["company_title"] else ""
+                    company = self.env["res.partner"].search([("name", "=", real_company_name)], limit=1)
+                    if not company:
+                        company = self.env["res.partner"].create({
+                            "name": real_company_name,
+                            "street": line["street"],
+                            "zip": line["country/code"].split("-")[1],
+                            "city": line["municipality"],
+                            "vat": line["TVA"],
+                            "phone": line["phone"],
+                            "mobile": line["mobile"],
+                            "email": line["email"],
+                            "comment": line["notes"]
+                        })
+                    if line["contact_name"]:
+                        company_contact = self.env["res.partner"].search([("parent_id", "=", company.id), ("name", "=", line["contact_name"])], limit=1)
+                        if not company_contact:
+                            company_contact = self.env["res.partner"].create({"name": line["contact_name"], "parent_id": company.id})
+                    if line["email_compta"]:
+                        compta_contact = self.env["res.partner"].search([("parent_id", "=", company.id), ("email", "=", line["email_compta"])], limit=1)
+                        if not compta_contact:
+                            compta_contact = self.env["res.partner"].create({
+                                "parent_id": company.id,
+                                "email": line["email_compta"]
+                            })
+                                   
+        except Exception as e:
+            _logger.error(e)
+            raise ValidationError('Le fichier xls est incorrect')
