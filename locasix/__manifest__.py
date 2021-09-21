cc3517ec1b66607ddab041b1ddc206e8536bf721
thherman <hermansthibault@gmail.com>
2021-09-21 10:57:06 +0200

    pdf

--- locasix/__manifest__.py
+++ locasix/__manifest__.py
@@ -6,7 +6,7 @@
         Locasix""",
 
     'description': """
-        Long description of module's purpose
+        Locasix
     """,
 
     'author': "istarii",
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'ERP',
-    'version': '0.17.70',
+    'version': '0.17.71',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale_management', 'product'],
