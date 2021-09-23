8c848d2f20d862ec38b835172e2c6f18210c3043
thherman <hermansthibault@gmail.com>
2021-09-23 15:01:29 +0200

    debug

--- locasix/models/assemblage_link.py
+++ locasix/models/assemblage_link.py
@@ -4,6 +4,6 @@ class AssemblageLink(models.Model):
     _name = "locasix.assemblage.link"
     _description = "Lien d'assemblage"
 
-    assemblage_product_id = fields.Many2One(string="Assemblage", comodel_name = "product.template")
+    assemblage_product_id = fields.Many2one(string="Assemblage", comodel_name = "product.template")
     linked_product_id = fields.Many2one(string="Composant", comodel_name="product.template")
 
