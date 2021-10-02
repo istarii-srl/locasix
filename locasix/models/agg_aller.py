0016b608f811913e44e436d56fce1574a341e4da
florvhee <florvhee@gmail.com>
2021-10-02 22:32:03 +0200

    debug

--- locasix/models/agg_aller.py
+++ locasix/models/agg_aller.py
@@ -118,17 +118,17 @@ class AggAller(models.Model):
             
             for aller in aggAller.aller_ids:
                 aller.create_copy_to_new_agg(new_agg)
-            view = self.env.ref('locasix.locasix_agg_aller_form')
+            view = self.env.ref('locasix.locasix_day_form')
             return {
-            'name': 'Allers',
+            'name': 'Allers et retours',
             'type': 'ir.actions.act_window',
             'view_type': 'form',
             'view_mode': 'form',
-            'res_model': 'locasix.agg.aller',
+            'res_model': 'locasix.day',
             'views': [(view.id, 'form')],
             'view_id': view.id,
-            'res_id': new_agg.id,
-            'target': 'new',
+            'res_id': newday_id.id,
+            'target': 'current',
             }      
 
 
