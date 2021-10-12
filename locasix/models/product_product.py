0fb791b2951a4c0622ecb0f1c6eaed654f766f43
florvhee <florvhee@gmail.com>
2021-10-12 17:19:41 +0200

    debug

--- locasix/models/product_product.py
+++ locasix/models/product_product.py
@@ -4,4 +4,23 @@ class ProductProduct(models.Model):
     _name = "product.product"
     _inherit = "product.product"
 
+
+    def get_plans(self):
+        for product in self:
+            if product.is_assemblage_product:
+                plans = product.plan_ids[:]
+                for assemblage in product.assemblage_ids:
+                    plans += assemblage.linked_product_id.get_plans()
+                return plans
+            else:
+                return product.plan_ids
     
+    def get_technicals(self):
+        for product in self:
+            if product.is_assemblage_product:
+                plans = product.technical_ids[:]
+                for assemblage in product.assemblage_ids:
+                    plans += assemblage.linked_product_id.get_technicals()
+                return plans
+            else:
+                return product.technical_ids
