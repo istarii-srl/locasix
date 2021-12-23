47652605f2088237021e28cb97b7cf510ea385ee
florvhee <florvhee@gmail.com>
2021-12-23 10:01:41 +0100

    debug

--- locasix/models/aller.py
+++ locasix/models/aller.py
@@ -86,7 +86,7 @@ class Aller(models.Model):
     def _state_selection(self):
         select = [("progress", "En cours"), ("cancel", "Annulé"), ("move", "Déplacé")]
         if self.env.user.has_group('locasix.group_locasix_admin'):
-            select.append(('zdone', "Fini"))
+            select.append(('zdone', "Fini"), ('a', "Statut technique"))
         return select
 
     @api.depends('localite_id', 'localite_id_depl', 'is_depl')
