83ab1a7fbadbb02f1e550d063ac9df8398894c01
florvhee <florvhee@gmail.com>
2022-01-20 14:53:05 +0100

    debug

--- locasix/migrations/0.18.428.py/pre-10.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, SUPERUSER_ID
-
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    allers = env["locasix.aller"].search([])
-    for aller in allers:
-        if aller.state =="progress":
-            aller.state = "aprogress"
