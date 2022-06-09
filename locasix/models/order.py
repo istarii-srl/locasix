239a0f0baff6d3ff10b97b483c7507c5b0bfa97d
florvhee <florvhee@gmail.com>
2022-06-09 15:02:30 +0200

    debug

--- locasix/models/order.py
+++ locasix/models/order.py
@@ -945,7 +945,8 @@ class Order(models.Model):
     def action_put_in_agenda(self):
         _logger.info("action put in agenda")
         for order in self:
-            line_ids = order.order_line.filtered(lambda x: x.product_id and x.product_id.type != "service")
+            excluded_lists = ["VIDANGE", "ASSM", "FNG", "CAU", "TAR", "TA", "TR", "SURC-TA", "SURC-TR", "SURC-TAR", "FN", "CEM", "MCI", "FASSA", "FASSR", "MAIR", "TH1", "SURC-TH1", "MCADRA", "MCADRR"]
+            line_ids = order.order_line.filtered(lambda x: x.product_id and x.product_id.type != "service" and x.product_id.default_code and x.product_id.default_code not in excluded_lists )
             view = self.env.ref('locasix.locasix_order_to_agenda_form')
             return {
             'name': 'Créer les allers et retours',
