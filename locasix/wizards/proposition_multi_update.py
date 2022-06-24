3273ff94e41717403bb8fe84a815535a57d0e655
florvhee <florvhee@gmail.com>
2022-06-24 10:48:35 +0200

    debug

--- locasix/wizards/proposition_multi_update.py
+++ locasix/wizards/proposition_multi_update.py
@@ -1,5 +1,7 @@
 from odoo import fields, api, models
 from odoo.exceptions import UserError
+import logging
+_logger = logging.getLogger(__name__)
 
 class PropositionMultiUpdate(models.TransientModel):
     _name = "locasix.proposition.multi.wizard"
@@ -17,12 +19,15 @@ class PropositionMultiUpdate(models.TransientModel):
     def validate(self):
         for wizard in self:
             agg_id = False
-            is_boss = wizard.user_id.has_group('locasix.group_locasix_admin') or wizard.user_id.has_group('locasix.group_locasix_agenda_resp')
+            is_boss = False
+            is_boss = bool(wizard.user_id.has_group('locasix.group_locasix_admin') or wizard.user_id.has_group('locasix.group_locasix_agenda_resp'))
+            _logger.info(is_boss)
             for prop in wizard.prop_ids:      
                 if not agg_id:
                     agg_id = prop.agg_id.id
                 elif agg_id != prop.agg_id.id:
                     raise UserError("Vous ne pouvez modifier des propositions que d'un même groupement")
+                _logger.info(prop.proposition_status)
                 if (prop.proposition_status != "pending_boss" and is_boss) or (prop.proposition_status != "pending_worker" and not is_boss):
                     raise UserError("Vous ne pouvez modifier que les propositions en attente de confirmation de la même personne (responsable ou demandeur)")
             i = 0
