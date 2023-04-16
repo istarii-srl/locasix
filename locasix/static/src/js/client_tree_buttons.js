383641123695eb4948c8da3b1adecd0999ac5bea
florvhee <florvhee@gmail.com>
2023-04-16 17:03:59 +0200

    debug

--- locasix/static/src/js/client_tree_buttons.js
+++ locasix/static/src/js/client_tree_buttons.js
@@ -1,27 +1,42 @@
-odoo.define("locasix.client_tree_view_button", function (require) {
+odoo.define("locasix.tree_button", function (require) {
   "use strict";
-
-  var ajax = require("web.ajax");
   var ListController = require("web.ListController");
-
-  ListController.include({
-    renderButtons: function ($node) {
-      this._super.apply(this, arguments);
+  var ListView = require("web.ListView");
+  var viewRegistry = require("web.view_registry");
+  var TreeButton = ListController.extend({
+    buttons_template: "locasix.client_buttons",
+    events: _.extend({}, ListController.prototype.events, {
+      "click .o_button_import_client": "_OpenWizard",
+    }),
+    _OpenWizard: function () {
       var self = this;
-      if (this.$buttons) {
-        $(this.$buttons)
-          .find(".o_button_import_client")
-          .on("click", function () {
-            self.do_action({
-              name: "Exporter les produits",
-              type: "ir.actions.act_window",
-              res_model: "locasix.client.import",
-              target: "new",
-              views: [[false, "form"]],
-              context: { is_modal: true },
-            }); //custom code
-          });
-      }
+      // this._rpc({
+      //   model: "mailing.mailing",
+      //   method: "action_create_from_salesforce_campaign",
+      // }).then(function (action) {
+      //   return self.do_action({
+      //     name: "Choose Salesforce campaign",
+      //     type: "ir.actions.act_window",
+      //     res_model: "salesforce.contact.import.wizard",
+      //     target: "new",
+      //     views: [[false, "form"]],
+      //     context: { is_modal: true },
+      //   });
+      // });
+      this.do_action({
+        name: "Exporter les produits",
+        type: "ir.actions.act_window",
+        res_model: "locasix.client.import",
+        target: "new",
+        views: [[false, "form"]],
+        context: { is_modal: true },
+      });
     },
   });
+  var LosixClientView = ListView.extend({
+    config: _.extend({}, ListView.prototype.config, {
+      Controller: TreeButton,
+    }),
+  });
+  viewRegistry.add("locasix_client_buttons", LosixClientView);
 });
