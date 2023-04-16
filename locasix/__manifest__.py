b4108c9c863d2c8eb973a676a1b6f2d09fb4bf83
florvhee <florvhee@gmail.com>
2023-04-16 11:54:52 +0200

    debug

--- locasix/__manifest__.py
+++ locasix/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'ERP',
-    'version': '16.0.0.43',
+    'version': '16.0.0.44',
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'sale_management', 'product', 'contacts'],
 
