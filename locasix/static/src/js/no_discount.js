1232314e0dbc125a022d0b7c8ae495a00aa9ace0
florvhee <florvhee@gmail.com>
2023-05-01 22:56:57 +0200

    debug

--- locasix/static/src/js/no_discount.js
+++ locasix/static/src/js/no_discount.js
@@ -3,7 +3,7 @@
 import { patch } from "@web/core/utils/patch";
 import ProductDiscountField from "@sale/js/product_discount_field";
 
-patch(ProductDiscountField.prototype, "debug mps", {
+patch(ProductDiscountField, "disable discount", {
     onChange(ev) {
         console.log("Test");
   },
