43e48d8457a363a36985234e30c2d3b5ce8bc14f
florvhee <florvhee@gmail.com>
2023-05-01 22:59:13 +0200

    debug

--- locasix/static/src/js/no_discount.js
+++ locasix/static/src/js/no_discount.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import ProductDiscountField from "@sale/js/product_discount_field";
+import {ProductDiscountField} from "@sale/js/product_discount_field";
 
-patch(ProductDiscountField, "disable discount", {
+patch(ProductDiscountField.prototype, "disable discount", {
     onChange(ev) {
         console.log("Test");
   },
