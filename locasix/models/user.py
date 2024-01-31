6f4cd5d6a075bbc16a2213b8c414fada71a8b894
Florvhee <florvhee@gmail.com>
2024-01-31 21:25:28 +0100

    debug

--- locasix/models/user.py
+++ locasix/models/user.py
@@ -10,4 +10,5 @@ class ResUsers(models.Model):
     _description = 'res.users'
 
     signature_image = fields.Image(string="Signature")
+    work_location = fields.Selection([('ghislain', 'Saint-Ghislain'), ('thimister', 'Thimister')], string="Lieu de travail", default='ghislain')
     
