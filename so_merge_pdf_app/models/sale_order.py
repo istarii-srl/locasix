da0a8eb5c1b31bf049fd40940983e8cb135e9e99
thherman <hermansthibault@gmail.com>
2024-10-11 14:52:02 +0200

    debug

--- so_merge_pdf_app/models/sale_order.py
+++ so_merge_pdf_app/models/sale_order.py
@@ -8,7 +8,7 @@ class SaleOrder(models.Model):
 
 
 	so_merge_report = fields.Boolean(string="Report To Merge" , compute='_compute_so_merge_report' , store=True)
-	so_merge_report_attachment = fields.Boolean(string="Afficher la pub SixUnits ?"  , store=True, default=True)
+	so_merge_report_attachment = fields.Boolean(string="Afficher la pub fin de doc ?"  , store=True, default=True)
 	attachment_ids = fields.Many2many('ir.attachment', string='Merge Attachments' )
 
 	
